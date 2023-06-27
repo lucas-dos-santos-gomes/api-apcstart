4ca847210675682109ced7256872518483c4496e
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-27 04:28:01 -0300

    Fazendo o CRUD do aluno

--- src/main/java/br/com/apcstart/ApcStart/controller/AlunoController.java
+++ src/main/java/br/com/apcstart/ApcStart/controller/AlunoController.java
@@ -1,14 +1,52 @@
 package br.com.apcstart.ApcStart.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.apcstart.ApcStart.model.Aluno;
 import br.com.apcstart.ApcStart.repository.AlunoRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
   @Autowired
   private AlunoRepository alunoRepository;
+
+  // CRUD - CREATE , READ , UPDATE , DELETE
+
+  // CREATE
+  @PostMapping
+  public String cadastrarAluno(@RequestBody Aluno aluno) {
+    alunoRepository.save(aluno);
+    return "Aluno cadastrado com sucesso!";
+  }
+
+  // READ
+  @GetMapping
+  public List<Aluno> mostrarAlunos() {
+    return alunoRepository.findAll();
+  }
+
+  // UPDATE
+  @PutMapping
+  public String atualizarAluno(@RequestBody Aluno aluno) {
+    alunoRepository.save(aluno);
+    return "Aluno atualizado com sucesso!";
+  }
+
+  // DELETE
+  @DeleteMapping("/{id}")
+  public String deletarAluno(@PathVariable("id") int id) {
+    alunoRepository.deleteById(id);
+    return "Aluno apagado com sucesso!";
+  }
 }
