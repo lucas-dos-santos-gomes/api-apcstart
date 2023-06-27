50f1496462b9c15fe62fefca0eb3621b5a050b22
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-26 21:18:41 -0300

    Primeiro commit do projeto

---
+++ src/main/java/br/com/apcstart/ApcStart/ApcStartApplication.java
@@ -0,0 +1,13 @@
+package br.com.apcstart.ApcStart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApcStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApcStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/apcstart/ApcStart/ApcStartApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apcstart.ApcStart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApcStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
