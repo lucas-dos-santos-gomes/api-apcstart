c77cf2d3fe50601c786204ca58bbad8d8d7d9400
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-06-29 06:00:20 -0300

    Criando repositório do psicopedagogo

---
+++ src/main/java/br/com/apcstart/ApcStart/repository/PsicopedagogoRepository.java
@@ -0,0 +1,8 @@
+package br.com.apcstart.ApcStart.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.apcstart.ApcStart.model.Psicopedagogo;
+
+public interface PsicopedagogoRepository extends JpaRepository<Psicopedagogo, Integer> {
+  
+}
