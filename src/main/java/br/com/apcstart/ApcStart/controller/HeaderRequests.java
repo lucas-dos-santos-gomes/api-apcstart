ff9bc3d2dc00c7b034ef467ef929e10c966b8d6b
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-02 04:32:59 -0300

    Configurando header

---
+++ src/main/java/br/com/apcstart/ApcStart/controller/HeaderRequests.java
@@ -0,0 +1,29 @@
+package br.com.apcstart.ApcStart.controller;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class HeaderRequests implements Filter {
+ 
+    @Override
+    public void doFilter(ServletRequest request,
+                        ServletResponse response,
+                        FilterChain chain) throws IOException, ServletException
+    {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+ 
+        res.addHeader("Access-Control-Allow-Origin", "*");
+        chain.doFilter(req, res);
+    }
+}
