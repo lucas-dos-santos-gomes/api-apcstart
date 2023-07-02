efa668c9ab770c5f8119ad95041b45233ab9ddcb
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-01 22:29:54 -0300

    Atualizando os controllers do pedagogo

--- src/main/java/br/com/apcstart/ApcStart/controller/PedagogoController.java
+++ src/main/java/br/com/apcstart/ApcStart/controller/PedagogoController.java
@@ -14,12 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.apcstart.ApcStart.model.Pedagogo;
 import br.com.apcstart.ApcStart.repository.PedagogoRepository;
+import br.com.apcstart.ApcStart.service.PedagogoService;
 
 @RestController
 @RequestMapping("/pedagogo")
 public class PedagogoController {
   @Autowired
   private PedagogoRepository pedagogoRepository;
+
+  @Autowired
+  private PedagogoService pedagogoService;
 
   @PostMapping
   public String cadastrarPedagogo(@RequestBody Pedagogo pedagogo) {
@@ -30,6 +34,16 @@ public class PedagogoController {
   @GetMapping
   public List<Pedagogo> mostrarPedagogos() {
     return pedagogoRepository.findAll();
+  }
+
+  @GetMapping("/{email}")
+  public Pedagogo mostrarPorEmail(@PathVariable String email) {
+    return pedagogoService.findByEmail(email);
+  }
+  
+  @GetMapping("/{email}/{senha}")
+  public String validarLogin(@PathVariable String email, @PathVariable String senha) {
+    return pedagogoService.findByLogin(email, senha);
   }
 
   @PutMapping
