e0c6bfde9e2701d1cef627df95393131bb3353fa
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-07-03 06:21:11 -0300

    Liberando qualquer acesso para a API

--- src/main/java/br/com/apcstart/ApcStart/config/WebConfig.java
+++ src/main/java/br/com/apcstart/ApcStart/config/WebConfig.java
@@ -9,6 +9,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    registry.addMapping("/**")
+    .allowedOrigins("*")
+    .allowedHeaders("*")
+    .allowedMethods("GET", "PUT", "POST", "DELETE");;
   }
 }
